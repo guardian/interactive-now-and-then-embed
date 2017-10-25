daaaa8ba80277da90a76afc0c4148bcb6c0da0b1
Garry Blight <garry.blight@guardian.co.uk>
2017-10-25 14:30:26 +0100

    Incorporated basic Daan code

--- src/js/config.js
+++ src/js/config.js
@@ -24,62 +24,65 @@ System.config({
     "core-js": "npm:core-js@1.2.6",
     "guardian/iframe-messenger": "github:guardian/iframe-messenger@master",
     "json": "github:systemjs/plugin-json@0.1.2",
+    "nouislider": "npm:nouislider@10.1.0",
     "reqwest": "github:ded/reqwest@1.1.5",
     "text": "github:systemjs/plugin-text@0.0.2",
     "github:jspm/nodelibs-assert@0.1.0": {
-      "assert": "npm:assert@1.4.0"
+      "assert": "npm:assert@1.4.1"
     },
-    "github:jspm/nodelibs-buffer@0.1.0": {
-      "buffer": "npm:buffer@3.6.0"
+    "github:jspm/nodelibs-buffer@0.1.1": {
+      "buffer": "npm:buffer@5.0.8"
     },
     "github:jspm/nodelibs-path@0.1.0": {
       "path-browserify": "npm:path-browserify@0.0.0"
     },
     "github:jspm/nodelibs-process@0.1.2": {
-      "process": "npm:process@0.11.3"
+      "process": "npm:process@0.11.10"
     },
     "github:jspm/nodelibs-util@0.1.0": {
       "util": "npm:util@0.10.3"
     },
-    "npm:assert@1.4.0": {
+    "github:jspm/nodelibs-vm@0.1.0": {
+      "vm-browserify": "npm:vm-browserify@0.0.4"
+    },
+    "npm:assert@1.4.1": {
       "assert": "github:jspm/nodelibs-assert@0.1.0",
-      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
-      "buffer-shims": "npm:buffer-shims@1.0.0",
+      "buffer": "github:jspm/nodelibs-buffer@0.1.1",
       "process": "github:jspm/nodelibs-process@0.1.2",
       "util": "npm:util@0.10.3"
     },
     "npm:babel-runtime@5.8.38": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:buffer-shims@1.0.0": {
-      "buffer": "github:jspm/nodelibs-buffer@0.1.0"
-    },
-    "npm:buffer@3.6.0": {
-      "base64-js": "npm:base64-js@0.0.8",
-      "child_process": "github:jspm/nodelibs-child_process@0.1.0",
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "ieee754": "npm:ieee754@1.1.6",
-      "isarray": "npm:isarray@1.0.0",
-      "process": "github:jspm/nodelibs-process@0.1.2"
+    "npm:buffer@5.0.8": {
+      "base64-js": "npm:base64-js@1.2.1",
+      "ieee754": "npm:ieee754@1.1.8"
     },
     "npm:core-js@1.2.6": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "path": "github:jspm/nodelibs-path@0.1.0",
-      "process": "github:jspm/nodelibs-process@0.1.2",
-      "systemjs-json": "github:systemjs/plugin-json@0.1.2"
+      "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
     },
+    "npm:nouislider@10.1.0": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
     "npm:path-browserify@0.0.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:process@0.11.3": {
-      "assert": "github:jspm/nodelibs-assert@0.1.0"
+    "npm:process@0.11.10": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0",
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "vm": "github:jspm/nodelibs-vm@0.1.0"
     },
     "npm:util@0.10.3": {
       "inherits": "npm:inherits@2.0.1",
       "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:vm-browserify@0.0.4": {
+      "indexof": "npm:indexof@0.0.1"
     }
   }
 });
--- src/js/embed.js
+++ src/js/embed.js
@@ -1,16 +1,122 @@
 import iframeMessenger from 'guardian/iframe-messenger'
-import reqwest from 'reqwest'
+//import reqwest from 'reqwest'
 import embedHTML from './text/embed.html!text'
+import noUiSlider from 'nouislider'
+
+var firstClick = true;
 
 window.init = function init(el, config) {
     iframeMessenger.enableAutoResize();
 
     el.innerHTML = embedHTML;
 
-    reqwest({
-        url: 'http://ip.jsontest.com/',
-        type: 'json',
-        crossOrigin: true,
-        success: (resp) => el.querySelector('.test-msg').innerHTML = `Your IP address is ${resp.ip}`
+    // reqwest({
+    //     url: 'http://ip.jsontest.com/',
+    //     type: 'json',
+    //     crossOrigin: true,
+    //     success: (resp) => el.querySelector('.test-msg').innerHTML = `Your IP address is ${resp.ip}`
+    // });
+
+	var metadata = document.location.search;
+
+    metadata = "mobile_before=//media.guim.co.uk/8a6284c68dc1bdaaa8d13f930b1d588679091f8d/0_0_4264_4301/496.jpg&desktop_before=//media.guim.co.uk/8a6284c68dc1bdaaa8d13f930b1d588679091f8d/0_0_4264_4301/991.jpg&label_before=Then&amp;mobile_after=//media.guim.co.uk/bf1ab4640975691dc677a79e89fe36757ceb2231/0_0_4264_4301/496.jpg&desktop_after=//media.guim.co.uk/bf1ab4640975691dc677a79e89fe36757ceb2231/0_0_4264_4301/991.jpg&label_after=Now&analytics_label=Berkley%20Square%20-%20Summer%20of%20Love%20Sliders%20-%2050%20years&";
+    var properties = {};
+
+    if (metadata) {
+        metadata.replace('?', '').split('&').forEach(function(property) {
+            properties[property.split('=')[0]] = property.split('=')[1]
+        })
+    }
+
+    // properties.desktop_before = "https://media.guim.co.uk/f55c177459f02193b0d19c083d3a5217a3b42d04/0_0_3702_2708/1000.jpg";
+    // properties.desktop_after = "https://media.guim.co.uk/bf1ab4640975691dc677a79e89fe36757ceb2231/0_0_4264_4301/991.jpg";
+    // properties.mobile_before = "";
+    // properties.mobile_after = "";
+    // properties.label_after = "Now";
+    // properties.label_before = "Then";
+
+
+    //el.innerHTML = template;
+
+    var elWidth = el.getBoundingClientRect().width;
+    var isMobile = elWidth < 480 ? true : false;
+    var photoSize = isMobile ? "mobile" : "desktop";
+
+    el.querySelector('#first-photo img').src = properties[photoSize + "_after"];
+    el.querySelector('#second-photo img').src = properties[photoSize + "_before"];
+
+    var slider = el.querySelector('#slider');
+    var sliderStateNow = el.querySelector('#slider-now');
+    var sliderStateThen = el.querySelector('#slider-then');
+
+    sliderStateNow.innerHTML = decodeURIComponent(properties.label_after);
+    sliderStateThen.innerHTML = decodeURIComponent(properties.label_before);
+
+    noUiSlider.create(slider, {
+        start: [0],
+        step: 0.05,
+        animate: true,
+        range: {
+            'min': 0,
+            'max': 1
+        }
+    })
+
+    var firstPhoto = el.querySelector('#first-photo');
+    slider.noUiSlider.on('update', function(values, handle) {
+        if (typeof ga !== 'undefined') {
+            fireAnalytics(properties);
+        }
+        firstPhoto.style.opacity = values;
+        sliderStateNow.style.opacity = values;
+        sliderStateThen.style.opacity = 1 - values;
     });
+
+    var photoContainer = el.querySelector('.fade-container');
+
+    var fadeTimeout;
+
+    photoContainer.addEventListener('click', function() {
+        fireAnalytics(properties);
+        clearTimeout(fadeTimeout);
+        var currentValue = parseFloat(slider.noUiSlider.get());
+        var targetValue = currentValue > 0.5 ? 0 : 1;
+        var addUp = targetValue > currentValue ? true : false;
+
+        function setSlider() {
+            fadeTimeout = setTimeout(function() {
+                if (addUp) {
+                    currentValue += 0.05;
+                } else {
+                    currentValue -= 0.05;
+                }
+
+                slider.noUiSlider.set(currentValue);
+
+                if (addUp && currentValue < targetValue) {
+                    setSlider();
+                } else if (!addUp && currentValue > targetValue) {
+                    setSlider();
+                }
+            }, 50)
+        }
+
+        setSlider();
+    })
+
+    
+
 };
+
+function fireAnalytics(properties) {
+    if (properties.label && firstClick) {
+        firstClick = false;
+        ga("send", "event", properties.label, 'transitioned');
+    }
+}
+
+
+
+
+
+
