21de6b509f1ab967cb4d9074f1b95ab629114910
Garry Blight <garry.blight@guardian.co.uk>
2018-08-29 11:22:06 +0100

    Added override for generator iframes

--- src/js/embed.js
+++ src/js/embed.js
@@ -35,7 +35,14 @@ window.init = function init(el, config) {
 
     interactiveType = properties["type"] || "fader";
 
-    iframeMessenger.getLocation(checkAndroidApp);
+    if (properties.override == undefined || properties.override == "") {
+
+        iframeMessenger.getLocation(checkAndroidApp);
+
+    } else {
+        buildApp( containerEl );
+        startWidget();
+    }
 
     // buildApp( containerEl ); // Uncomment if testing locally
 
@@ -288,17 +295,26 @@ function checkIfInView(d) {
     var threshold = 300;
 
     if (d.iframeTop < (d.innerHeight - threshold) && interactiveType == "slider") {
-        document.querySelector('#second-photo').classList.add("slider-transition-initial");
-        document.querySelector('#slider-2').classList.add("fade-in");
-        document.querySelector('#second-photo').classList.remove("gv-hide");
-        var origin = containerEl.querySelector('.noUi-origin');
-        origin.classList.add("origin-smooth");
+        // document.querySelector('#second-photo').classList.add("slider-transition-initial");
+        // document.querySelector('#slider-2').classList.add("fade-in");
+        // document.querySelector('#second-photo').classList.remove("gv-hide");
+        // var origin = containerEl.querySelector('.noUi-origin');
+        // origin.classList.add("origin-smooth");
         // sld.updateOptions({
         //     start: [50]
         // });
+        startWidget();
         clearInterval(intervalCheck);
     }
 
+}
+
+function startWidget() {
+    document.querySelector('#second-photo').classList.add("slider-transition-initial");
+    document.querySelector('#slider-2').classList.add("fade-in");
+    document.querySelector('#second-photo').classList.remove("gv-hide");
+    var origin = containerEl.querySelector('.noUi-origin');
+    origin.classList.add("origin-smooth");
 }
 
 function fireAnalytics(properties) {
